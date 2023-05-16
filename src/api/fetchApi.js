34502e747a89d5f319fa162c1711ccfa3b21bd26
White Bull <blueded88@gmail.com>
2023-05-16 16:57:11 -0400

    Make little changes

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// http://api.weatherapi.com/v1/current.json?key=59848614b07c4c0685d152839230205&q=lagos&aqi=yes
-// http://api.weatherapi.com/v1/forecast.json?key=59848614b07c4c0685d152839230205&q=lagos&days=7
-
 export async function getApi(link, location) {
     const res = await axios.get(`${link}?key=${process.env.API_KEY}&q=${location}&aqi=yes`)
     if(res.status == 200) return res;
